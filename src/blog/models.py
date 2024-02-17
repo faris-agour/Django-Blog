1a770cb19b3717d56b352bf4923179a8d6811fd3
fares_agour <faresabouagour@gmail.com>
2024-02-18 00:28:50 +0200

    add comments field

--- src/blog/models.py
+++ src/blog/models.py
@@ -41,3 +41,13 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-created']
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_comment")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    content = models.CharField(max_length=400)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.user.username} on {self.post.title}'
