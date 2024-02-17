e2997ab4bb65d1b2d1326359ebd3fc67cbc21459
fares_agour <faresabouagour@gmail.com>
2024-02-18 01:06:40 +0200

    add paginator

--- src/blog/views.py
+++ src/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Post
 from django.views.generic import ListView, DetailView
+from django.core.paginator import Paginator
 
 
 # def index(request):
@@ -9,6 +10,9 @@ from django.views.generic import ListView, DetailView
 #     return render(request, "index.html", {"posts": posts})
 def blog_list(request):
     posts = Post.objects.all()
+    paginator = Paginator(posts,2)
+    page_number = request.GET.get("page")
+    posts = paginator.get_page(page_number)
     return render(request, 'index.html', {'posts': posts})
 
 
