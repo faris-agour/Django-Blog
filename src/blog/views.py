6c90614ed19f0eeb16e465df512df9adaba7da2e
fares-agour <faresabouagour@gmail.com>
2024-02-21 17:40:35 +0200

    add search field

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,28 +1,31 @@
-from django.db.models import Count
+from django.core.paginator import Paginator
+from django.db.models import Count, Q
 from django.shortcuts import render, get_object_or_404
 
 from .models import Post
-from django.views.generic import ListView, DetailView
-from django.core.paginator import Paginator
 
 
 # def index(request):
 #     posts = Post.objects.all()
 #     return render(request, "index.html", {"posts": posts})
 def blog_list(request):
-    posts = Post.objects.all()
-    paginator = Paginator(posts,2)
+    all_posts = Post.objects.all()
+    paginator = Paginator(all_posts, 3)
     page_number = request.GET.get("page")
-    posts = paginator.get_page(page_number)
-    return render(request, 'index.html', {'posts': posts})
+    paginated_posts = paginator.get_page(page_number)
+    query = request.GET.get('q')
+    if query:
+        paginated_posts = all_posts.filter(Q(title__icontains=query) | Q(body__icontains=query))
+
+    return render(request, 'index.html', {'posts': paginated_posts})
 
 
 def post_details(request, slug):
     post = get_object_or_404(Post, slug=slug, status="CR")
-    post_tags =post.tags.all()
+    post_tags = post.tags.all()
     similar_posts = Post.objects.filter(tags__in=post_tags).exclude(slug=slug).distinct()
-    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by("-same_tags","-created")[:4]
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by("-same_tags", "-created")[:4]
 
     return render(request,
                   'details.html',
-                  {'post': post, "similar_posts":similar_posts})
+                  {'post': post, "similar_posts": similar_posts})
