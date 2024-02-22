501db45884df739363db3152341ad7c740d6cf43
fares-agour <faresabouagour@gmail.com>
2024-02-22 18:02:47 +0200

    fix some bugs

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 
 from .models import Post, Comment
+from django.db import models
+from django import forms
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'created', 'status']
+    formfield_overrides = {
+        models.TextField: {'widget': forms.Textarea(attrs={'class': 'ckeditor'})},  # Use CKEditor for rich text editing
+    }
 
 
 @admin.register(Comment)
